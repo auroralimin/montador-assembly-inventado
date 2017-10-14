5b29d4ce9b332f810911ac140085c44590587e3a
Aurora Li Min <auroralimin@hotmail.com>
2017-10-14 02:34:23 -0300

    Automatiza testes
    Implementa um script simples que confere se o pre-processamento
    esperado é o que está sendo feito para os dados exemplos.

---
+++ test/num.asm
@@ -0,0 +1,13 @@
+L1:  EQU 1
+L2:  EQU 0xA
+L4:  EQU -9
+L3: CONST L2
+L5: CONST L4
+SECTION 0x0923k
+IF L1
+LOAD SPACE ;faz esta opera ̧c~ao se L1 for verdadeiro
+IF LABELERRADA
+IF L2
+INPUT SPACE ;faz esta opera ̧c~ao se L2 for verdadeiro
+SECTION DATA
+N: SPACE
